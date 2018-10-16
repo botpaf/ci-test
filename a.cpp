dc8ba0a641bf806ed53ccca6ccfd329ba173cc56
botpaf <botpaf@example.com>
2018-10-16 19:21:00 +0200

    add test for <optional>

--- a.cpp
+++ a.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <optional>
 #include <tuple>
 
 int main() {
   auto a = std::tuple(3.14, "^__^");
   auto[f, b] = a;
   std::cout << f << " " << b << std::endl;
+
+  auto o = std::optional("happy happy joy joy");
+  std::cout << *o << std::endl;
+
   return 0;
 }
